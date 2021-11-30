6dd840d106a9bc916f631df23849fe9624e2488e
Naseem Ganji <naseem.ganji@icloud.com>
2021-11-30 01:26:32 -0500

    flatten.js

--- flatten.js
+++ flatten.js
@@ -12,6 +12,4 @@ const flatten = function(array) {
   return flattenedArray;
 };
 flatten([1, 2, [3, 4], 5, [6]]) // => [1, 2, 3, 4, 5, 6]
-
-
-//module.exports = flatten;
+module.exports = flatten;
